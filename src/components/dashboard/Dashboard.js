8327e97de9af3c52a057ea25720b64ec8f902f2f
Riley Benson <ryguybenson@yahoo.com>
2019-05-21 14:56:25 -0700

    add thunk

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList'
 import Notifications from './Notifications'
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
   render(){
+    console.log(this.props)
     return (
       <div>
         <div>
@@ -19,4 +21,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+
+import authReducer from './authReducer'
+
+import projectReducer from './projectReducer'
+
+import { combineReducers } from 'redux'
+
+
+
+const rootReducer = combineReducers({
+
+  auth: authReducer,
+
+  project: projectReducer
+
+});
+
+
+
+export default rootReducer
+
