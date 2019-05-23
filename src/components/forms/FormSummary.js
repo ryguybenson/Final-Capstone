4c1271af7516b583fd1a9657c0115454bbc16c96
Riley Benson <ryguybenson@yahoo.com>
2019-05-23 07:32:00 -0700

    add card panel to form

--- src/components/forms/FormSummary.js
+++ src/components/forms/FormSummary.js
@@ -21,22 +21,22 @@ const FormSummary = ({form}) => {
       </div>
       <div style={text}>
         <p>How do you plan on enjoying your portraits (you may change your mind at anytime, it just gives me a better idea of what types of images to create for you)?</p>
-        <p>{form.answerOne}</p>
+        <p>A: {form.answerOne}</p>
         <p>Describe your personality in as many adjectives as are fitting (shy, fun, outgoing, athletic, girly girl, guys guy, jokester, laid back, studious, sweet, obnoxious, loud, nature lover, urban, introvert, extrovert, home body, rule follower, adventurous, etc...)</p>
-        <p>{form.answerTwo}</p>
+        <p>A: {form.answerTwo}</p>
         <p>Please let me know your preferred locations fot the shoot or what type of location/background you would like (urban, river, meadow, trees, brick wall, your yard, sports field, your school, barn, junk yard, bridge, etc...)</p>
-        <p>{form.answerThree}</p> 
+        <p>A: {form.answerThree}</p> 
         <p>Please provide a brief description of the outfits you'll be wearing. A couple things to leep in mind when selecting yout outfits:
               <br/>
               *Select outfits that you're comfortable in; you dont want to be worrying about straps that won't stay in place or other wardrobe issues.
               <br/>
               *Select clothes that best reflect your personality and dont forget accessories!
             </p>
-        <p>{form.answerFour}</p>
+        <p>A: {form.answerFour}</p>
         <p>Will you be bringing any props with you (sports equipment, and instrument, books, a pet, family or friends, etc...)</p>
-        <p>{form.answerFive}</p>
+        <p>A: {form.answerFive}</p>
         <p>Please describe or attach pics of poses that you love and share any other thoughts and desires you have for your shoot</p>
-        <p>{form.answerSix}</p>
+        <p>A: {form.answerSix}</p>
       </div>
       <Link to={'/form/' + form.id} key={form.id}>
         <button form={form}>Change</button>
